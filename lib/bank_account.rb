0e17c0a1a0be951b365193524ec0b2121255e45f
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-22 02:13:13 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,9 +16,16 @@ def display_balance
   "Your balance is $#{balance}."
 end
 
+def valid?
+  if @balance > 0 && @status == "open"
+     true
+  else
+    false
+  end
+end
+
 def close_account
   @status = "closed"
 end
 
-end
 end
